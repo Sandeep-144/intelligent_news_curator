0ad121dbe8b0cfc51a15fee44bbbd89325e9ec7f
Sandeep-144 <149867097+Sandeep-144@users.noreply.github.com>
2024-10-10 23:01:18 +0530

    Delete static directory

--- static/script.js
+++
@@ -1,7 +0,0 @@
-// Toggle Navigation Menu on Hamburger Click
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('navLinks');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
